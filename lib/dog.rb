c8b0ea774519041fb731d3d77f5d33fc69e03644
Kristy Peterson <kpeterson.kap@gmail.com>
2017-11-20 02:23:28 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -88,5 +88,14 @@ def self.find_by_name(name)
   end.first
 end
 
+def update
+  sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+  DB[:conn].execute(sql, self.name, self.breed, self.id)
+end
+
+
+
+end
+
 
 end
