f0e93f0278e255af14b3fb489018b3c94d1c387f
Kristy Peterson <kpeterson.kap@gmail.com>
2017-11-20 01:43:17 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -48,13 +48,13 @@ def self.create(hash)
   dog
 end
 
-def self.find_by_id(id:)
-  sql =  <<-SQL
+def self.find_by_id(id)
+
     SELECT * FROM dogs WHERE id = ?
-    SQL
     
+
   result = DB[:conn].execute(sql, id)[0]
-  Dog.new(result[0], result[1], result[2])
+  Dog.new(result{})
 
 end
 
